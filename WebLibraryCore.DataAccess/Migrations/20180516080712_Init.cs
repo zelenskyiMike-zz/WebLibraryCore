2fb2d8b0cc40f1e2d89f7782e50807edfa4906d1
MikeZelenskiy <zelenskiymo@gmail.com>
2018-05-16 11:08:29 +0300

    Created EFDbContexzt

---
+++ WebLibraryCore.DataAccess/Migrations/20180516080712_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebLibraryCore.DataAccess.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    ArticleID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ArticleGenreID = table.Column<int>(nullable: false),
+                    ArticleName = table.Column<string>(nullable: false),
+                    DateOfArticlePublish = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.ArticleID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
