7f2f8293162ccb05b3d437f839bd17462ad49d65
MikeZelenskiy <zelenskiymo@gmail.com>
2018-05-21 13:04:24 +0300

    AuthoMaper set-upped

---
+++ WebLibraryCore.BusinessLogic/MappingInitailizer.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace WebLibraryCore.BusinessLogic
+{
+    public class MappingInitailizer
+    {
+        public static void InitializeAutoMapper()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.AddProfile<AuthorMappingProfile>();
+                //cfg.AddProfile<BookMappingProfile>();
+                //cfg.AddProfile<BookGenresMappingProfile>();
+                //cfg.AddProfile<ArticleMappingProfile>();
+                //cfg.AddProfile<MagazineMappingProfile>();
+                //cfg.AddProfile<PublicationMappingProfile>();
+                //cfg.AddProfile<PublicationGenresMappingProfile>();
+            });
+
+        }
+    }
+}
---
+++ WebLibraryCore.BusinessLogic/MappingProfiles/BookMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLibraryCore.BusinessLogic.MappingProfiles
+{
+    public class BookMappingProfile
+    {
+
+    }
+}
